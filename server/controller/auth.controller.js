cbbffce8c21be34c95889f26c0fddbc3c7ab22cf
Saad Ahmed <saadahmed4324@gmail.com>
2024-02-06 12:01:31 +0500

    created route for google authentication

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -35,3 +35,41 @@ export const login = async (req, res, next) => {
     next(error);
   }
 };
+
+export const googleAuth = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = Jwt.sign({ id: user._id }, "eqoiruvfjdaf239412");
+      const { password: pass, ...rest } = user._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 12);
+
+      const newUser = new User({
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-4),
+        email: req.body.email,
+        password: hashedPassword,
+        avatar: req.body.photo,
+      });
+      await newUser.save();
+      const token = Jwt.sign({ id: newUser._id }, "eqoiruvfjdaf239412");
+      const { password: pass, ...rest } = newUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, signup } from "../controller/auth.controller.js";
+import { login, signup, googleAuth } from "../controller/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/sign-up", signup);
 router.post("/log-in", login);
+router.post("/google", googleAuth);
 
 export default router;
