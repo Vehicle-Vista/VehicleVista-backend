edf518084aad34df119faf36e93ba14251290c85
Saad Ahmed <saadahmed4324@gmail.com>
2024-02-07 22:46:43 +0500

    changed user avatar image address

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       default:
-        "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=",
+        "https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper-thumbnail.png",
     },
   },
   { timestamps: true }
