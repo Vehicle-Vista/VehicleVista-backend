6be18b57e65640687980e02bcbde786137e95f21
Saad Ahmed <saadahmed4324@gmail.com>
2024-02-09 22:05:21 +0500

    created endpoint for creating a new listing

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 const app = express();
 
@@ -24,6 +25,8 @@ app.use(cookieParser());
 
 app.use("/server/user", userRouter);
 app.use("/server/auth", authRouter);
+app.use("/server/listing", listingRouter);
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
---
+++ server/models/listing.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    modelYear: {
+      type: Number,
+      required: true,
+    },
+    mileage: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    imageURLs: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+export default Listing;
